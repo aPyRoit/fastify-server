c822db2118c00c20b02a9c58a66037a827e28473
aPyRoit <art_mal_YT@mail.ru>
2026-04-18 05:36:36 +0300

    mvp

--- src/index.js
+++ src/index.js
@@ -2,23 +2,63 @@ import fastify from 'fastify'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import fastifyStatic from '@fastify/static'
+import view from '@fastify/view'
+import pug from 'pug'
+import users from './users.js'
+import formbody from '@fastify/formbody';
 
 const app = fastify()
 const port = 3000
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-app.register(fastifyStatic, {
-  root: __dirname,
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+await app.register(fastifyStatic, {
+  root: dirname,
 })
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html')
+await app.register(formbody);
+
+console.log(users)
+
+await app.register(view, { engine: { pug } })
+
+app.get('/users', (req, res) => {
+  res.view('src/views/users/index', {users})
 })
+
+app.get('/users/create', (req, res) => {
+  res.view('src/views/users/form')
+});
+
+app.get(`/users/:id`, (req, res) => {
+  const user = users.find(u => u.id == req.params.id)
+  if (!user) {
+    return res.code(404).send('User not found')
+  }
+  else {
+    res.view('src/views/users/show', {user})
+  }
+})  
 
 app.get('/api', (req, res) => {
     res.send('Запрос прошел успешно')
 })
+
+app.post('/users', (req, res) => {
+  const { username, email } = req.body;
+  
+  const newUser = {
+    id: crypto.randomUUID(),
+    username,
+    email,
+  };
+  
+  users.push(newUser);
+  res.redirect('/users');
+});
+
+console.log('=== МАРШРУТЫ ЗАРЕГИСТРИРОВАНЫ ===');
+console.log(app.printRoutes());  // Покажет все маршруты
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/users.js
@@ -0,0 +1,503 @@
+const users = [
+  {
+    id: 'b438b6fa-765b-4706-8b22-88adb9b5534a',
+    username: 'Hortense.Kunde',
+    email: 'Tyreek.Muller@yahoo.com'
+  },
+  {
+    id: 'd47f891d-9854-46ae-88a9-9ad1c3391eab',
+    username: 'Lauretta.Cronin69',
+    email: 'Kayden_Walsh35@yahoo.com'
+  },
+  {
+    id: '46bf5c9b-2635-48a1-825a-d8d6552d5898',
+    username: 'Yadira52',
+    email: 'Desmond.Schoen73@yahoo.com'
+  },
+  {
+    id: '9df16378-14e9-47cb-a0bd-2c44a1aeb77c',
+    username: 'Arvel92',
+    email: 'Newell_Stokes90@hotmail.com'
+  },
+  {
+    id: '1266813c-7c08-4e93-8b6a-0a0b7181afc9',
+    username: 'Maxine_Beatty',
+    email: 'Hannah.Davis84@yahoo.com'
+  },
+  {
+    id: '6f3ee178-5031-43ce-804f-8a07f617f5cc',
+    username: 'Imogene.Jenkins',
+    email: 'Albina.Kuvalis99@yahoo.com'
+  },
+  {
+    id: '53134a45-0582-494a-80e0-2c0bfdbf6d5e',
+    username: 'Trisha.Stamm59',
+    email: 'Adan_Fahey@yahoo.com'
+  },
+  {
+    id: '939a467e-9f88-407e-9306-5d0eae9c556c',
+    username: 'Deangelo.Upton63',
+    email: 'Ethel70@gmail.com'
+  },
+  {
+    id: '63142195-93a3-44b8-a58b-e93bc3d7c35f',
+    username: 'Ollie80',
+    email: 'Ashleigh.Welch@gmail.com'
+  },
+  {
+    id: '363d74e5-e0b7-4700-b5f9-59325d84fd5f',
+    username: 'Hal.Jakubowski8',
+    email: 'Grayce.Wilkinson@gmail.com'
+  },
+  {
+    id: '65d40ed4-811d-46af-8300-be768da3c229',
+    username: 'Meagan_Gottlieb',
+    email: 'Noelia73@gmail.com'
+  },
+  {
+    id: '0474c29f-e30f-4ee6-9b36-d2e764e8cde4',
+    username: 'Laurianne.Watsica',
+    email: 'Zachery.Ernser14@hotmail.com'
+  },
+  {
+    id: 'c80f90a6-29ba-4388-9b5c-a08d928c1d28',
+    username: 'Delores52',
+    email: 'Alfonso.Schinner44@hotmail.com'
+  },
+  {
+    id: '4f6eaddf-bcb9-4bb0-b8c3-766187a800bb',
+    username: 'Selmer.Murazik',
+    email: 'Manuel_Krajcik3@gmail.com'
+  },
+  {
+    id: '4fe2fc8e-e6aa-4527-b45f-7a5fa31ff903',
+    username: 'Sebastian.Hickle11',
+    email: 'Rahsaan76@yahoo.com'
+  },
+  {
+    id: '2f399fa9-8c10-4f75-95c6-c7d1774fe713',
+    username: 'Jazmin87',
+    email: 'Leopoldo.West72@gmail.com'
+  },
+  {
+    id: 'e1bf8399-a2e9-40a3-a08d-d1ab4f418b35',
+    username: 'Sheridan52',
+    email: 'Jewell89@hotmail.com'
+  },
+  {
+    id: 'ec364a5c-1c9d-4698-bfc8-2e3ae33dd542',
+    username: 'Robyn.Langworth5',
+    email: 'Una.Cronin24@yahoo.com'
+  },
+  {
+    id: 'a7518564-680d-4e84-bf6b-b63f1734d513',
+    username: 'Whitney.Padberg',
+    email: 'Elvie.Labadie64@yahoo.com'
+  },
+  {
+    id: '33f7c799-fa30-4376-acc1-f896adafbd0a',
+    username: 'Barry.Morar',
+    email: 'Aisha30@gmail.com'
+  },
+  {
+    id: 'cb188be5-6ba8-4532-811d-60e343f1bead',
+    username: 'Margot69',
+    email: 'Josue.Kling@yahoo.com'
+  },
+  {
+    id: '2170f660-6005-4d70-b2bb-c55c6b9511e7',
+    username: 'Frank.Heller63',
+    email: 'Hayden.Kshlerin@yahoo.com'
+  },
+  {
+    id: '76893890-836d-4570-84a5-34f17082a4b5',
+    username: 'Eleanore.Kunze',
+    email: 'Emily.Marquardt@gmail.com'
+  },
+  {
+    id: '2c1eb706-ad96-47d5-a17f-630259a0e328',
+    username: 'Elody_Grimes88',
+    email: 'Maxime.Schuster74@gmail.com'
+  },
+  {
+    id: '31267eab-14f8-4c01-810e-4fb4fd22a191',
+    username: 'Uriah.Marvin44',
+    email: 'Amiya.Parisian52@hotmail.com'
+  },
+  {
+    id: '39472211-8ccf-4227-8dc8-26b509bb2566',
+    username: 'German40',
+    email: 'Wellington_Pacocha99@hotmail.com'
+  },
+  {
+    id: 'be0c1e92-0d2f-41d6-8dcd-7b50305700a1',
+    username: 'Jeromy.Wolff11',
+    email: 'Bailey.Graham@gmail.com'
+  },
+  {
+    id: 'd4bb0f2b-6df3-4d8f-ba93-b4df0f57ec31',
+    username: 'Ned.Hoppe46',
+    email: 'Milford_Stracke75@hotmail.com'
+  },
+  {
+    id: 'bfef0933-dc58-430e-9b80-66222913f3c9',
+    username: 'Laurianne_Kuhic42',
+    email: 'Leonor75@yahoo.com'
+  },
+  {
+    id: 'bcdf4782-74d5-410d-bde1-66d44b2ab23b',
+    username: 'Velda.Lowe43',
+    email: 'Queen28@hotmail.com'
+  },
+  {
+    id: '7908b015-f30c-4d8e-b240-9e91ba2de649',
+    username: 'Mohamed_Runte81',
+    email: 'Deangelo_Durgan95@gmail.com'
+  },
+  {
+    id: '1d9371f2-cfb1-492d-baf2-781a956c5fe1',
+    username: 'Margie83',
+    email: 'Dagmar45@yahoo.com'
+  },
+  {
+    id: '4cc1ed9b-5727-4c82-8db0-cd861d055e7e',
+    username: 'Roscoe.Lind49',
+    email: 'Odessa_Parisian5@hotmail.com'
+  },
+  {
+    id: 'b6aebe2c-b000-48c6-a02f-3d58725f9784',
+    username: 'Monserrat74',
+    email: 'Loren.Cole@hotmail.com'
+  },
+  {
+    id: '32f571ea-8603-4d93-9359-88dddb64f09d',
+    username: 'Wilfred.Dooley89',
+    email: 'Leola_Marquardt59@gmail.com'
+  },
+  {
+    id: 'df4d45c2-0621-4924-ab1f-48c3536bb27b',
+    username: 'Lou.Stroman',
+    email: 'Mabel13@yahoo.com'
+  },
+  {
+    id: 'fb1b61fb-9d5a-4718-8a72-1ade6fd66510',
+    username: 'Dereck_Crona',
+    email: 'Milo_Hilll74@gmail.com'
+  },
+  {
+    id: '0ade2943-b67d-438d-929d-ce0b38c6bdf8',
+    username: 'Giovani5',
+    email: 'Shaun95@gmail.com'
+  },
+  {
+    id: 'aa276100-b774-4822-9d0e-5e159f1c9af5',
+    username: 'Kailey_Hahn8',
+    email: 'Nels28@gmail.com'
+  },
+  {
+    id: '4648d38a-049d-43ed-b6dd-411e7a6cdeae',
+    username: 'Donna_Gislason',
+    email: 'Agustina70@yahoo.com'
+  },
+  {
+    id: '163b6043-1853-4e52-88ec-243e4279201a',
+    username: 'Destinee_Swaniawski38',
+    email: 'Mertie74@yahoo.com'
+  },
+  {
+    id: '3bc4e134-13c4-4829-b931-ba7efbf22f68',
+    username: 'Leila47',
+    email: 'Rafaela_Mosciski@gmail.com'
+  },
+  {
+    id: 'e2b4b30c-d2c5-499e-99d6-a3be6a80ec61',
+    username: 'Julia.Sipes20',
+    email: 'Kasandra.Greenfelder89@yahoo.com'
+  },
+  {
+    id: 'ef079050-974c-485e-bf2f-9e400607bab9',
+    username: 'Dax43',
+    email: 'Yasmine90@hotmail.com'
+  },
+  {
+    id: '6bf37c9c-427a-4991-973b-899416493352',
+    username: 'Retha.Brekke61',
+    email: 'Mekhi.Breitenberg@yahoo.com'
+  },
+  {
+    id: 'c20e5586-4bcd-4cb3-9b4b-1660eaca5822',
+    username: 'Luigi_Ryan67',
+    email: 'Roscoe54@yahoo.com'
+  },
+  {
+    id: 'b473991a-10d3-47fa-85d3-7990687dbe32',
+    username: 'Madison8',
+    email: 'Dale_Tremblay64@gmail.com'
+  },
+  {
+    id: '5c5d3b1d-af02-400d-baa6-062ee4008d5a',
+    username: 'Lou.Lubowitz',
+    email: 'Elliot53@hotmail.com'
+  },
+  {
+    id: '65db1bf7-7b35-45f2-b274-d6ad4b64e972',
+    username: 'Lowell.Baumbach',
+    email: 'Damian63@hotmail.com'
+  },
+  {
+    id: 'c645f6c1-6520-44c4-8d9c-fa54fb837cfa',
+    username: 'Jamel.Langosh',
+    email: 'Jeanne_Klocko28@hotmail.com'
+  },
+  {
+    id: 'd52264b5-922e-4de7-b14e-65487564330f',
+    username: 'Baylee_Johns82',
+    email: 'Kaia.Wisozk53@gmail.com'
+  },
+  {
+    id: '51ef855e-96c8-4334-a049-c1f70f447522',
+    username: 'Antoinette_Heaney6',
+    email: 'Felipa82@gmail.com'
+  },
+  {
+    id: 'd74a51dd-7712-4cd0-9c86-7b01df36bace',
+    username: 'Julie18',
+    email: 'Bridie.Senger@yahoo.com'
+  },
+  {
+    id: 'c14d2682-bcad-4204-bd2c-e8e3bc791413',
+    username: 'Alexandra.Ledner',
+    email: 'Janis17@gmail.com'
+  },
+  {
+    id: '5b76aae1-89aa-450b-807b-4012aaf5220d',
+    username: 'Skyla2',
+    email: 'Eli72@gmail.com'
+  },
+  {
+    id: '3654b482-da89-43d1-8e9f-4ea0191ba2a9',
+    username: 'Melisa.Streich',
+    email: 'Jacky.Metz63@yahoo.com'
+  },
+  {
+    id: '819bc9f5-0d78-4d45-b1a3-d9e0f68a7862',
+    username: 'Preston7',
+    email: 'Noemy83@gmail.com'
+  },
+  {
+    id: '02da4710-bbbe-4464-9aa6-e7ebfc3b3437',
+    username: 'Syble34',
+    email: 'Dangelo.Stroman99@gmail.com'
+  },
+  {
+    id: '5ea72c5c-9951-49b9-89b0-3924030d980d',
+    username: 'Paolo.Schuppe54',
+    email: 'Brennon.Feil@hotmail.com'
+  },
+  {
+    id: 'ae062836-5bb6-43cd-a83c-5e4f6c3996bd',
+    username: 'Raphael_Cormier19',
+    email: 'Lavina14@yahoo.com'
+  },
+  {
+    id: '1fcb3c08-4dc7-4713-ab1c-f179d25d82de',
+    username: 'Waylon59',
+    email: 'Lyric.Jaskolski65@hotmail.com'
+  },
+  {
+    id: '67acb98d-d04e-4921-b1c3-32984acf764c',
+    username: 'Layne.Pfeffer22',
+    email: 'Lowell_Baumbach@hotmail.com'
+  },
+  {
+    id: 'b27004ae-604f-41cb-99ea-fdcf951b86fe',
+    username: 'Tyrique_DuBuque9',
+    email: 'Reece_Rogahn@yahoo.com'
+  },
+  {
+    id: 'd89363ba-c94f-4a7c-b4c5-1c3a4a2938d7',
+    username: 'Caden_Corwin',
+    email: 'Cathryn_Veum@hotmail.com'
+  },
+  {
+    id: '377f6d28-998c-4212-a93b-1efce6238a91',
+    username: 'Pat.Koelpin6',
+    email: 'Kurt10@yahoo.com'
+  },
+  {
+    id: '24274f05-9576-41ad-a827-0e3e9fd00b8c',
+    username: 'Darius66',
+    email: 'Cornelius33@gmail.com'
+  },
+  {
+    id: '4aa949b6-44ea-4c9a-84e8-c81176e2ae6e',
+    username: 'Ben77',
+    email: 'Scottie58@gmail.com'
+  },
+  {
+    id: '58d286d2-e496-4e38-a3c4-fe70fa7912a0',
+    username: 'Tevin_Macejkovic',
+    email: 'Adan.Flatley@gmail.com'
+  },
+  {
+    id: '8499397f-b2e7-4dd0-b3ec-23943f78a16e',
+    username: 'Jerome78',
+    email: 'Abigail_Wunsch61@gmail.com'
+  },
+  {
+    id: '0745d117-6186-4f11-b25a-fe71a60d87bd',
+    username: 'Esmeralda9',
+    email: 'Adelia34@hotmail.com'
+  },
+  {
+    id: 'b5c48e68-c37e-4abf-b9df-aa27b510a07e',
+    username: 'Millie.Rohan47',
+    email: 'Darwin_Doyle18@hotmail.com'
+  },
+  {
+    id: '0407069e-8805-465e-a63c-d5db15d5d796',
+    username: 'Jude.Wisoky15',
+    email: 'Cortez_Weber95@yahoo.com'
+  },
+  {
+    id: '7e7b2365-ad5c-4460-b219-a08c87a69edb',
+    username: 'Elinor_Greenfelder48',
+    email: 'Joany42@hotmail.com'
+  },
+  {
+    id: 'b66b6940-ba10-4023-9d6a-f7c6b4e07f21',
+    username: 'Bridget_Morar97',
+    email: 'Jalyn18@hotmail.com'
+  },
+  {
+    id: 'f6e68bce-7998-4b41-8868-954a6dce129b',
+    username: 'Albertha_Satterfield',
+    email: 'Lisa_Langosh@yahoo.com'
+  },
+  {
+    id: 'c46b7fd4-08de-41e7-9fc2-8edfdf89165e',
+    username: 'Sheldon_McCullough9',
+    email: 'Lauretta_Reichel34@yahoo.com'
+  },
+  {
+    id: '1d8b1d91-795b-49dd-90ab-2ed2eacc21a5',
+    username: 'Breana_Marquardt55',
+    email: 'Reanna.Veum@gmail.com'
+  },
+  {
+    id: 'ccd853f9-f769-4fd6-806c-0d45e5775167',
+    username: 'Rey.Ferry',
+    email: 'Yadira.Reynolds39@yahoo.com'
+  },
+  {
+    id: '5179477a-9f89-4852-9038-050a8eabee59',
+    username: 'Kenny6',
+    email: 'Alberto_Stanton59@gmail.com'
+  },
+  {
+    id: 'ad57aca2-86aa-4121-a180-c9b7ed732da4',
+    username: 'Eloise82',
+    email: 'Destany_Gusikowski11@hotmail.com'
+  },
+  {
+    id: '072f5cd1-a54c-444c-856b-2b5d902c30ba',
+    username: 'Lisa.Weimann17',
+    email: 'Jany32@yahoo.com'
+  },
+  {
+    id: '0957c038-9239-4b77-864f-a544589a099e',
+    username: 'Alta.Maggio',
+    email: 'Diego.Marquardt@yahoo.com'
+  },
+  {
+    id: 'fefaaa04-4c2b-464e-b4cc-650720924bbf',
+    username: 'Jordy_Heaney54',
+    email: 'Davin6@gmail.com'
+  },
+  {
+    id: '189be558-385e-44a6-8fe5-0475a55e4984',
+    username: 'Fidel53',
+    email: 'Della.Stracke29@gmail.com'
+  },
+  {
+    id: '94a182ab-cced-4c3e-872c-c7b1163dd247',
+    username: 'Blaise15',
+    email: 'Abbey34@yahoo.com'
+  },
+  {
+    id: '856aec5c-20b1-46d6-b00e-da0c61cb35f1',
+    username: 'Mac4',
+    email: 'Kelli6@gmail.com'
+  },
+  {
+    id: 'c62bba47-0574-494a-99d3-cd0650cecb53',
+    username: 'Barney90',
+    email: 'Mohamed_Renner@gmail.com'
+  },
+  {
+    id: '1a759d3a-bf38-4b0d-95a6-ec7fc6e881e6',
+    username: 'Xzavier_Buckridge23',
+    email: 'Kale40@yahoo.com'
+  },
+  {
+    id: '3b88fdd0-ff18-401d-8047-4e5bc0634609',
+    username: 'Arturo_Heaney80',
+    email: 'Kaela12@hotmail.com'
+  },
+  {
+    id: 'a008192a-5b3a-4f74-8539-88a02302da20',
+    username: 'Mia_Leuschke23',
+    email: 'Viola_Rogahn22@gmail.com'
+  },
+  {
+    id: 'e43e2420-2782-4b89-b53c-58be7f8c1196',
+    username: 'Kiana54',
+    email: 'Lina16@yahoo.com'
+  },
+  {
+    id: '24921a05-3e91-4652-8b2d-f1aa8aeca633',
+    username: 'Salvatore.Buckridge72',
+    email: 'Walter_Bauch@yahoo.com'
+  },
+  {
+    id: 'a9d78f83-2d5f-4cf8-80d9-d4d342218a3e',
+    username: 'Bernardo.Powlowski98',
+    email: 'Earnest_Rau@gmail.com'
+  },
+  {
+    id: '5611b07a-71c7-4429-9e4d-a0d6d3837e8f',
+    username: 'Lee_Romaguera-Pouros33',
+    email: 'Leif_Runolfsdottir@gmail.com'
+  },
+  {
+    id: '8dcc89df-cc2d-4f43-b206-22bcba80ffec',
+    username: 'Laury_Schuster',
+    email: 'Kaylie_Nienow4@gmail.com'
+  },
+  {
+    id: 'fa436fbf-8638-4a82-82c3-12e9c3a38183',
+    username: 'Jerrod.Prohaska28',
+    email: 'Ezequiel45@gmail.com'
+  },
+  {
+    id: '89baebf5-f942-48fd-bb0c-bd165cc85120',
+    username: 'Thaddeus51',
+    email: 'Willow1@hotmail.com'
+  },
+  {
+    id: 'b4867989-58d5-4475-ae9b-01661c97ab57',
+    username: 'Abigail.Hudson',
+    email: 'Deanna45@hotmail.com'
+  },
+  {
+    id: 'a10f8b2b-ae45-49e4-a15a-f2c38b1c150d',
+    username: 'Cassandra79',
+    email: 'Kylee_Altenwerth@gmail.com'
+  },
+  {
+    id: '314ec713-f248-454a-a271-45a74a7b6b93',
+    username: 'Brianne.Pollich',
+    email: 'Emanuel41@hotmail.com'
+  }
+]
+export default users
